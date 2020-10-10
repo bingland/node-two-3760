c43d1ef2c326160631dbdf7f96444595a0a9326f
Brayden England <bray.patriot@gmail.com>
2020-10-09 22:40:49 -0600

    Added handlebars, changed index to handlebars file

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use('/', express.static('public'))
+// sets the app to use handlebars as the engine
+app.set('view engine', 'handlebars')
 
-//app.get('/', (req, res) => res.send('hi'));
+// handlebars configurations 
+app.set('views', __dirname + '/public')
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/public',
+}))
 
-app.listen(port, () => console.log(`App listening to port ${port}`));
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('main', {
+        title: 'Handlebars Assignment', 
+        headerInfo: 'Assignment for DGM 3760; Made with Express Handlebars'
+    })
+});
+app.listen(port, () => console.log(`App running on port ${port}...`))
